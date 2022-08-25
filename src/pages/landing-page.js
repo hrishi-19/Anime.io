af7789a728f66fa9cfc97f425564ec70ef99fa81
hrishi-19 <hrishakamath.com>
2022-08-25 12:31:23 +0530

    screens->pages

---
+++ src/pages/landing-page.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/header';
+import MainBody from '../components/main-body';
+import Contents from '../components/contents';
+
+export default function Landing() {
+    return (
+        <div>
+            <Header />
+            <MainBody />
+           <Contents/>
+        </div>
+    )
+}
