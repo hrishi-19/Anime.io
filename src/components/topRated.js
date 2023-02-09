0cc2f15069dee4152a457bf98041e03195083707
hrishi-19 <hrishakamath.com>
2023-02-09 13:34:16 +0530

    removed lazy component

--- src/components/topRated.js
+++ src/components/topRated.js
@@ -1,8 +1,8 @@
-import { React, useState, useEffect, lazy, Suspense } from "react";
+import { React, useState, useEffect } from "react";
 import axios from "axios";
 import '../css/scroller.css';
 import DummyBox from "./dummybox";
-const AnimeCard = lazy(() => import('../components/animeCard'))
+import AnimeCard from "./animeCard";
 
 
 export default function TopAnime() {
@@ -14,9 +14,10 @@ export default function TopAnime() {
     }
     useEffect(() => {
         getData().then(topdata => {
+            // console.log(topdata)
             setTop(topdata.data)
         })
-           
+
 
 
     }, [])
@@ -25,23 +26,23 @@ export default function TopAnime() {
             <div className="flex w-full flex-col px-5 py-6 ">
                 <p className="text-2xl w-9/12 sm:w-6/12 text-slate-700 font-semibold px-2 py-3 border-b-2">Top Rated</p>
                 <div className="wrapper ">
-                {topList && (
-                
-                  
+                    {topList && (
+
+
                         topList.map((top, index) => {
-                            return <Suspense fallback={<DummyBox />}>
-                                <AnimeCard id={top.mal_id} img={top.images.webp.large_image_url} title={top.title} score={top.score} key={index} />
-                            </Suspense>
+                            return <AnimeCard key={top.mal_id} id={top.mal_id} img={top.images.webp.large_image_url} title={top.title} score={top.score} />
+                            
+
                         })
-                    
-                
-            )}
-            {!topList && [1,2,3,4,5].map(n=><DummyBox key={n}/>)}
-             </div>
-        </div>
+
+
+                    )}
+                    {!topList && [1, 2, 3, 4, 5].map(n => <DummyBox key={n} />)}
+                </div>
+            </div>
         </div>
 
-           
-            
+
+
     )
 }
